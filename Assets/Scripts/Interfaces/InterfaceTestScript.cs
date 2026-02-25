3732daeaaee9da9e51ae619ec1c836bd74e9a244
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-02-25 13:56:23 +0200

    interface

---
+++ Assets/Scripts/Interfaces/InterfaceTestScript.cs
@@ -0,0 +1,98 @@
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        IShape trapezium = new Trapezium();
+        IShape circle = new Circle();
+        IShape nonagon = new Nonagon();
+
+        Debug.Log("Trapezium Area: " + trapezium.CalculateArea());
+        Debug.Log("Circle Area: " + circle.CalculateArea());
+        Debug.Log("Nonagon Area: " + nonagon.CalculateArea());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public interface IShape
+{
+    float CalculateArea();
+    float CalculatePerimeter();
+}
+
+public class Trapezium : IShape
+{
+
+    float base1 = 5f;
+    float base2 = 7f;
+    float height = 4f;
+    float side1 = 3f;
+    float side2 = 6f;
+
+    public void CalculateUnknownSides()
+    {
+        Debug.Log("CalculatingUnknownSides");
+    }
+
+    public float CalculateArea()
+    {
+        return ((base1 + base2) / 2) * height;
+    }
+
+    public float CalculatePerimeter()
+    {
+        return base1 + base2 + side1 + side2;
+    }
+}
+
+public class Circle : IShape
+{
+
+    float radius = 3f;
+
+    public float CalculateArea()
+    {
+        return Mathf.PI * radius * radius;
+    }
+
+    public void CalculateRadius()
+    {
+        Debug.Log("Calculated Radius is: " + radius);
+    }
+
+    public float CalculatePerimeter()
+    {
+        return 2 * Mathf.PI * radius;
+    }
+}
+
+public class Nonagon : IShape
+{
+    int numberOfSides = 9;
+    float sideLength = 4f;
+
+    public float CalculateArea()
+    {
+        return 100f;
+    }
+
+    public float CalculatePerimeter()
+    {
+        return numberOfSides * sideLength;
+    }
+
+    public int CalculateNumberOfSides()
+    {
+        return numberOfSides;
+    }
+}
